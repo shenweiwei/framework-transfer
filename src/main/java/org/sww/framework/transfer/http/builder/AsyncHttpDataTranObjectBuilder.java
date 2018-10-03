c168294a41e246a10f32ac77aa48c4b88e9266b7
Shen weiwei <shenweiwei9@163.com>
2018-10-04 01:20:02 +0800

    init

--- src/main/java/org/sww/framework/transfer/http/builder/AsyncHttpDataTranObjectBuilder.java
+++ src/main/java/org/sww/framework/transfer/http/builder/AsyncHttpDataTranObjectBuilder.java
@@ -3,12 +3,10 @@ package org.sww.framework.transfer.http.builder;
 import org.sww.framework.transfer.http.dto.AsyncHttpDataTransferObject;
 
 public class AsyncHttpDataTranObjectBuilder extends HttpDataTranObjectBuilder {
-
 	@SuppressWarnings("unchecked")
 	@Override
-	public AsyncHttpDataTransferObject build() {
+	public AsyncHttpDataTransferObject<?, ?> build() {
 		// TODO Auto-generated method stub
-		return (AsyncHttpDataTransferObject)super.build();
+		return (AsyncHttpDataTransferObject<?, ?>) super.build();
 	}
-		
 }
