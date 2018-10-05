1a92618b7eee586f19bc20823ba1855cb95fa729
Shen weiwei <shenweiwei9@163.com>
2018-10-05 22:24:53 +0800

    init

--- src/main/java/org/sww/framework/transfer/dto/AbstractDTO.java
+++ src/main/java/org/sww/framework/transfer/dto/AbstractDTO.java
@@ -3,20 +3,20 @@ package org.sww.framework.transfer.dto;
 /**
  * @author Administrator
  */
-public abstract class AbstractDTO<I, O> implements DataTransferObject<I, O> {
-	private InputDTO<I> inputDTO;
-	private OutputDTO<O> outputDTO;
+public abstract class AbstractDTO implements DataTransferObject {
+	private InputDTO inputDTO;
+	private OutputDTO outputDTO;
 
-	public InputDTO<I> getInputDTO() {
+	public InputDTO getInputDTO() {
 		return inputDTO;
 	}
-	public void setInputDTO(InputDTO<I> inputDTO) {
+	public void setInputDTO(InputDTO inputDTO) {
 		this.inputDTO = inputDTO;
 	}
-	public OutputDTO<O> getOutputDTO() {
+	public OutputDTO getOutputDTO() {
 		return outputDTO;
 	}
-	public void setOutputDTO(OutputDTO<O> outputDTO) {
+	public void setOutputDTO(OutputDTO outputDTO) {
 		this.outputDTO = outputDTO;
 	}
 }
